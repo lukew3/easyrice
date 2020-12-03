9746090bc270884eb668c039052fd10ec17acc4b
lukew3 <lukew25073@gmail.com>
2020-12-02 19:42:32 -0500

    add documentation and dictionaries to pkg_install

--- easyrice/commands/mod_pkg_install.py
+++ easyrice/commands/mod_pkg_install.py
@@ -1,8 +1,45 @@
 import os
 
-def get_install_script(package, distro_name):
+git_url_dict = {
+    'bspwm': 'https://github.com/baskerville/bspwm.git',
+    'sxhkd': 'https://github.com/baskerville/sxhkd.git',
+}
 
-    pass
+# Types guide is as follows:
+"""
+d: debian 'apt' repo available
+f: fedora 'dnf' repo available
+u: ubuntu 'apt-get' repo available
+# Arch support is necessary, but I don't understand it much right now, I think aur is how you use it though
+a: arch 'aur' repo available
+
+#Install from source methods
+0: No install from source available
+1: `make & sudo make install` in top directory
+
+"""
+install_type_dict = {
+    'bspwm': [1],
+    'sxhkd': [1],
+    ''
+}
+
+def get_install_script(package, distro_name):
+    git_url = git_url_dict[package]
+    install_method = install_type_dict[package]
+    install_method_line = ''
+
+    if install_method[0] == 1:
+        install_method_line = 'make & sudo make install'
+    script_list = [
+        'git clone ' + clone_url,
+        'cd ' + package_name,
+        install_method_line,
+        'cd ..',
+        'rm -rf ' + package_name
+    ]
+    script = '\n'.join(script_list)
+    return script
 
 
 def bspwm(distro):
